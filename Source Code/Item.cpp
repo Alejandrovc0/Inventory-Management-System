0c096343e157e37033eba1ce99f2872320049230
Juan's laptop <alejovalencia2000@gmail.com>
2023-12-02 17:56:50 -0500

    Fix setters and getters in Item class

--- Source Code/Item.cpp
+++ Source Code/Item.cpp
@@ -6,4 +6,43 @@ using namespace std;
 Item::Item() : quantity(0), price(0.0) {}
 
 Item::Item(string name, int quantity, double price)
-    : name(name), quantity(quantity), price(price) {}
+{
+    this->setName(name);
+    this->setQuantity(quantity);
+    this->setPrice(price);
+};
+
+string Item::getName()
+{
+    return name;
+}
+
+int Item::getQuantity()
+{
+    return quantity;
+}
+
+double Item::getPrice()
+{
+    return price;
+}
+
+void Item::setName(string& Newname)
+{
+    name = Newname;
+}
+
+void Item::setQuantity(int newQuantity)
+{
+    quantity = Newquantity;
+}
+
+void Item::setPrice(double newPrice)
+{
+    price = Newprice;
+}
+
+void Item::displayItem()
+{
+    cout << "Name: " << name << "\tQuantity: " << quantity << "\tPrice: " << price << endl;
+}
